72a3ac50e2ea81c4b4821a05e1679381b3a1e9de
Matteo Scurati <matteo.scurati@gmail.com>
2017-03-10 00:33:07 +0100

    Template

---
+++ Thorfile
@@ -0,0 +1,30 @@
+require 'thor/group'
+
+module Middleman
+  class Generator < ::Thor::Group
+    include ::Thor::Actions
+
+    source_root File.expand_path(File.dirname(__FILE__))
+
+    def copy_default_files
+      directory 'template', '.', exclude_pattern: /\.DS_Store$/
+    end
+
+    def ask_about_datocms_params
+      if yes?('Do you want to configure DatoCMS?')
+        @token = ask('Please insert your DatoCMS site read-only token:')
+        @base_url = ask('What will be the base url of your site? (eg. https://www.mysite.com)')
+      end
+
+      template 'optional/config.rb', 'config.rb'
+    end
+
+    def setup_bemo
+      run 'yarn'
+      run './node_modules/.bin/bemo-scaffold -s source/assets/stylesheets/'
+      run 'yarn run webfonts'
+      remove_file 'source/assets/stylesheets/application.sass'
+      template 'optional/application.sass', 'source/assets/stylesheets/application.sass'
+    end
+  end
+end
---
+++ optional/config.rb
@@ -0,0 +1,88 @@
+page '/*.xml', layout: false
+page '/*.json', layout: false
+page '/*.txt', layout: false
+
+set :url_root, '<%= @base_url %>'
+
+ignore '/templates/*'
+
+set :css_dir, 'assets/stylesheets'
+set :js_dir, 'assets/javascripts'
+set :images_dir, 'assets/images'
+
+activate :directory_indexes
+activate :pagination
+<%- if @token -%>
+activate :dato,
+  token: '<%= @token %>',
+  base_url: '<%= @base_url %>'
+<%- end -%>
+
+proxy "/index.html", "/templates/index.html"
+
+activate :external_pipeline,
+  name: :webpack,
+  command: build? ?
+    "./node_modules/webpack/bin/webpack.js -p" :
+    "./node_modules/webpack/bin/webpack.js --watch -d --progress --color",
+  source: ".tmp/dist",
+  latency: 1
+
+configure :build do
+  activate :minify_html do |html|
+    html.remove_input_attributes = false
+  end
+  activate :search_engine_sitemap,
+    default_priority: 0.5,
+    default_change_frequency: 'weekly'
+  activate :asset_hash
+  activate :gzip
+  activate :relative_assets
+end
+
+after_build do
+  File.rename 'build/redirects', 'build/_redirects'
+end
+
+configure :development do
+  activate :livereload
+end
+
+#helpers
+helpers do
+  def markdown(text)
+    renderer = Redcarpet::Render::HTML.new
+    Redcarpet::Markdown.new(renderer).render(text)
+  end
+end
+
+<%- if @token -%>
+# dato.articles.each do |article|
+#   proxy(
+#     '/articles/#{article.slug}.html',
+#     '/templates/article.html',
+#     locals: { article: article }
+#   )
+# end
+# paginate(
+#   dato.articles.sort_by(&:published_at).reverse,
+#   '/articles',
+#   '/templates/articles.html'
+# )
+# MULTILANG SAMPLES
+# [:en, :it].each do |locale|
+#   I18n.with_locale(locale) do
+#     dato.aritcles.each do |article|
+#       I18n.locale = locale
+#       proxy "/#{locale}/articles/#{article.slug}/index.html", "/templates/article_template.html", :locals => { article: article }, ignore: true, locale: locale
+#     end
+#   end
+# end
+
+# [:en, :it].each do |locale|
+#   I18n.with_locale(locale) do
+#     I18n.locale = locale
+#     paginate dato.articles.select{|a| a.published == true}.sort_by(&:date).reverse, "/#{I18n.locale}/articles", "/templates/articles.html", locals: { locale: I18n.locale }
+#   end
+# end
+<%- end -%>
